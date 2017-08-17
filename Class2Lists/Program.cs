ea91c24cfab3ef7cbc11eab1907132f24168aaa8
Chris Bay <chris@chrisbay.net>
2017-08-17 16:52:29 -0500

    Fix VS 2017 migration

---
+++ Class2Lists/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Class2Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ten = new List<int>();
+            
+        }
+    }
+}
