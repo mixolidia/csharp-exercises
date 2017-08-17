26e41f931ca6e3e04b3502681644b2df03bc3216
Chris Bay <chris@chrisbay.net>
2017-08-17 16:52:29 -0500

    Fix VS 2017 migration

---
+++ Area/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double r = -1;
+            double r;
+            double a;
+            string input;
+
+            Console.WriteLine("Enter a radius: ");
+            input = Console.ReadLine();
+            r = double.Parse(input);
+
+            while (r < 0)
+            {
+                Console.WriteLine("Please enter a number larger than 0.\nEnter a radius: ");
+                input = Console.ReadLine();
+                r = double.Parse(input);
+            }
+
+            a = Math.PI * r * r;
+            Console.WriteLine("The are of a circle with radius " + r + " is: " + a);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NumericTypeRectangleArea/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NumericTypeRectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double w;
+            double l;
+            double rr;
+
+            Console.WriteLine("To get the area of a rectangle we need the width and length." +
+                "\n What is the width?");
+            w = Console.ReadLine();
+            Console.WriteLIne("What is the lenght?");
+            l = Console.ReadLine();
+
+            Console.ReadLine();
+        }
+    }
+}
